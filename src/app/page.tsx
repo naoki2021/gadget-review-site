5fa3ac596ce0a00478b5cd23d10d4e82ce546866
naoki <naoki2021n@gmail.com>
2025-11-27 02:57:26 +0900

    fix: revalidate=0を追加して確実に動的レンダリングにする

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { getAllProducts } from '@/lib/contentful';
 import Link from 'next/link';
 
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 const categories = [
   {
